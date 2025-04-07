c2c5d572d6d490f3292a849bc4db1e35871b56b6
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-04-07 11:37:27 +0200

    updated tests

--- product-service/src/test/java/com/easycar/product_service/controller/ProductControllerTest.java
+++ product-service/src/test/java/com/easycar/product_service/controller/ProductControllerTest.java
@@ -15,7 +15,7 @@ import java.math.BigDecimal;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ProductController.class)
 public class ProductControllerTest {
@@ -42,6 +42,9 @@ public class ProductControllerTest {
         mockMvc.perform(post("/api/products")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(productDto)))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.statusCode").value("201"))
+                .andExpect(jsonPath("$.statusMsg").value("Product created successfully"));
     }
 }
