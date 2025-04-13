82126614ef29c7bb2c0cc812555536a58ff9e69e
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-04-13 09:50:54 +0200

    updated swagger ui description for product controller

--- product-service/src/main/java/com/easycar/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/easycar/product_service/controller/ProductController.java
@@ -8,16 +8,15 @@ import com.easycar.product_service.dto.ResponseDto;
 import com.easycar.product_service.service.ProductService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.enums.ParameterStyle;
 import io.swagger.v3.oas.annotations.enums.Explode;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -45,28 +44,26 @@ public class ProductController {
     @Parameters({
         @Parameter(
                 name = "page",
-                description = "page number",
+                description = "page number (0-based)",
                 in = ParameterIn.QUERY,
                 schema = @Schema(type = "integer", defaultValue = "0")),
         @Parameter(
                 name = "size",
                 description = "page size",
                 in = ParameterIn.QUERY,
-                schema = @Schema(type = "integer", defaultValue = "5")),
+                schema = @Schema(type = "integer", defaultValue = "100")),
         @Parameter(
                 name = "sort",
-                description = "sort specification",
+                description = "sort specification by comma-separated value (e.g. 'price,asc')",
                 in = ParameterIn.QUERY,
                 schema = @Schema(type = "array"),
                 explode = Explode.FALSE,
-                style = ParameterStyle.SIMPLE
-        ),
+                style = ParameterStyle.SIMPLE),
     })
     public ResponseEntity<PageDto<ProductDto>> getProducts(
-            @RequestParam(required = false) BigDecimal minPrice,
-            @RequestParam(required = false) BigDecimal maxPrice,
-            @ParameterObject
-            @PageableDefault(size = 100) Pageable pageable) {
+            @Parameter(description = "Min price (inclusive)") @RequestParam(required = false) BigDecimal minPrice,
+            @Parameter(description = "Max price (inclusive)") @RequestParam(required = false) BigDecimal maxPrice,
+            @ParameterObject Pageable pageable) {
         PageDto<ProductDto> products = productService.findProducts(minPrice, maxPrice, pageable);
         return ResponseEntity.status(HttpStatus.OK).body(products);
     }
