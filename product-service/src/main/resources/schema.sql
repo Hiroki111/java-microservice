b81277b3720f1716d300b888982621782e6ff0c1
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-04-15 10:12:08 +0200

    Updated initial sql statements

--- product-service/src/main/resources/schema.sql
+++ product-service/src/main/resources/schema.sql
@@ -1,7 +1,19 @@
+CREATE TABLE IF NOT EXISTS dealers (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS products (
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  description VARCHAR(100) NOT NULL,
-  price DECIMAL NOT NULL,
-  available BOOLEAN NOT NULL
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    price DECIMAL NOT NULL,
+    available BOOLEAN NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    dealer_id BIGINT NOT NULL,
+    CONSTRAINT fk_products_dealer
+        FOREIGN KEY (dealer_id)
+        REFERENCES dealers(id)
+        ON DELETE CASCADE
 );
