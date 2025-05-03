d6a32b773897f92d0566a4be4dddfa7d8a94a2c9
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-05-03 09:11:07 +0200

    fixed order submit endpoint

--- order-service/src/main/java/com/easycar/order_service/dto/OrderCreateDto.java
+++ order-service/src/main/java/com/easycar/order_service/dto/OrderCreateDto.java
@@ -3,6 +3,7 @@ package com.easycar.order_service.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -10,10 +11,11 @@ import lombok.Data;
 public class OrderCreateDto {
     @Schema(example = "1")
     @Min(value = 1)
+    @NotNull(message = "Product ID cannot be null")
     private Long productId;
 
     // TODO: Replace it with customer ID
     @Schema(example = "John Smith")
-    @NotBlank(message = "customerName cannot be empty")
+    @NotBlank(message = "Customer name cannot be empty")
     private String customerName;
 }
