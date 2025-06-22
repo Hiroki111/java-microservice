f590701d714eaa06955a2d1ac5f3835ad076ede8
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-06-22 14:11:51 +0200

    introduced flyway to the order service

--- order-service/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS orders (
-    id SERIAL PRIMARY KEY,
-    product_id BIGINT NOT NULL,
-    customer_id VARCHAR(36) NOT NULL,
-    customer_name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP
-);
