63a0139a2a561b2c46a4f92d045b990f95566098
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-08-25 08:39:58 +0200

    updated gatewayserver for kubernetes service discover

--- gatewayserver/src/main/java/com/easycar/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/easycar/gatewayserver/GatewayserverApplication.java
@@ -3,11 +3,13 @@ package com.easycar.gatewayserver;
 import java.time.LocalDateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayserverApplication {
 
     public static void main(String[] args) {
@@ -26,13 +28,13 @@ public class GatewayserverApplication {
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
                                 .circuitBreaker(config -> config.setName("orderServiceCircuitBreaker"))
                         )
-                        .uri("lb://ORDER-SERVICE"))
+                        .uri("lb://http://order-service:8082"))
                 .route(p -> p
                         .path("/easycar/product-service/**")
                         .filters(f -> f.rewritePath("/easycar/product-service/(?<segment>.*)", "/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
                         )
-                        .uri("lb://PRODUCT-SERVICE"))
+                        .uri("lb://http://product-service:8081"))
                 .build();
     }
 }
