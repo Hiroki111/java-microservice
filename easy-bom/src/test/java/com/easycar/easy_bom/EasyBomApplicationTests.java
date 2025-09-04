5a91decf386ed4d67f59213f62f023a5ca20f0ad
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-09-04 12:07:25 +0200

    imported easy-bom

---
+++ easy-bom/src/test/java/com/easycar/easy_bom/EasyBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easycar.easy_bom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
