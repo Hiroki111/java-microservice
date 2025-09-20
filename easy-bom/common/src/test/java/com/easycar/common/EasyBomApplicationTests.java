29f302f4d2005853ae7b69ec9de4c473abaf655c
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-09-20 13:47:57 +0200

    fixed testing

--- easy-bom/common/src/test/java/com/easycar/common/EasyBomApplicationTests.java
+++ easy-bom/common/src/test/java/com/easycar/common/EasyBomApplicationTests.java
@@ -1,9 +1,9 @@
 package com.easycar.common;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@ContextConfiguration
 class EasyBomApplicationTests {
 
 	@Test
